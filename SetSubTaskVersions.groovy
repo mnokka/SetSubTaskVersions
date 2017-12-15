c0c11a3241ce766216c1650b44f38878780423d1
Mika Nokka <mika.nokka@ambientia.fi>
2017-12-15 16:26:49 +0200

    Cleaned. Still works

--- SetSubTaskVersions.groovy
+++ SetSubTaskVersions.groovy
@@ -5,16 +5,12 @@
  1) When creating subtask, parent task Fiexed Version(s) should be copied to child task
  2) When parent task is edited, same should happe
  
- To be implemented as a JIRA listener (for Issue Created event and XXXX )
+ To be implemented as a JIRA listener (for Issue Created event and Parent Issue edited )
  
  
  DEC 20117 mika.nokka1@gmail.com
  */
  
- 
- // CONFIGURATIONS
- //def SIMULATION=1 // 0=normal usage 1=simulation, only system log comments made
- // END OF CONFIGURATIONS
  
  
  import com.atlassian.jira.component.ComponentAccessor
@@ -49,12 +45,12 @@
  
  // set logging to Jira log
  def log = Logger.getLogger("SetSubTaskVersions") // change for customer system
- log.setLevel(Level.DEBUG)
+ log.setLevel(Level.INFO) // DEBUG INFO
   
  
  log.debug("---------- SetSubTaskVersions started -----------")
- log.debug("Event:"+event)
- log.debug("Event: ${event.getEventTypeId()} fired for ${event.issue} and caught by listener")
+ //log.debug("Event:"+event)
+ //log.debug("Event: ${event.getEventTypeId()} fired for ${event.issue} and caught by listener")
  //log.debug("ISSUE_CREATED_ID VALUE: ${EventType.ISSUE_CREATED_ID}")
  
  def issue = event.issue as Issue //from Adaptavista examples
@@ -62,55 +58,19 @@
  if (issue.isSubTask()) {       /* || issue.parentObject.statusObject.name == "Open" */
 	   log.debug("This is subtask event")
 	   parent=issue.getParentObject()
-	   log.debug("Parent:${parent} ")
-	   summary=parent.getSummary()
-	   log.debug("Parent Summary:${summary} --> subtask:${issue}")
 	   Collection versions=parent.getFixVersions()
-	  // Collection<Version> versions = new ArrayList<Version>();
-	  // fixVersions = fix_Issue.getFixVersions()
 	   
 	   if (versions==null) {
-		   log.debug("null")
+		   log.info("Parent(${parent}) has no FixedVersions set")
 	   }
 	   else {
-		   log.debug("NOT null")
-		   log.debug("Parent FixedVersions:${versions}")
+		   log.info("Parent(${parent}) FixedVersions:${versions} setting for subtask:${issue}")
 		   
-		   //issue.setFixVersions([versions])
-		   //issue.store() // needed to store changes
-		   //ApplicationUser user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
-		   
-
-		   //issueManager.updateIssue(event.getUser(), issue, EventDispatchOption.ISSUE_CREATED, false)
-		   //reIndex(issue)
-		   
-		   // ei kaadu, ei vahihda fixed versionia
-		   //MutableIssue mutableIssue = issue
-		   //mutableIssue.setFixVersions([versions])
-		   //mutableIssue.store()
-		   //issue.setFixVersions([versions])
-		   //issue.store()
-		   
-		   //tämä failaa eventissä
 		   MutableIssue mutableIssue = issueManager.getIssueObject(event.issue.id)
 		   mutableIssue.setFixVersions(versions)
-		   issueManager.updateIssue(event.getUser(),mutableIssue, EventDispatchOption.ISSUE_UPDATED, false)
-		   
-		   // failaa eventissä
-		   //issue.setFixVersions( ["TestiReleaseDev2017"] )
-		   //issue.store()
-		   //issueManager.updateIssue(event.getUser(),issue, EventDispatchOption.ISSUE_UPDATED, false)
-		   
-		   
-		 //  CustomField subfield = customFieldManager.getCustomFieldObjectByName('Fix Version/s')
-		 //  def changeHolder = new DefaultIssueChangeHolder()
-		 // subfield.updateValue(null, isue, new ModifiedValue(ChildCustomDueDate,tempdate),changeHolder)
-		   
-		   
-		   
+		   issueManager.updateIssue(event.getUser(),mutableIssue, EventDispatchOption.ISSUE_UPDATED, false)   
 	   }
 	   
-
  }
  else {
 		log.debug("This is NOT subtask event. Maybe normal task was created")
